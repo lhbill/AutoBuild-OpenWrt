92bdd1c68e2dd1904bcf57caf8539c3bfbef24c8
lhbill <34673835+lhbill@users.noreply.github.com>
2021-04-20 08:39:52 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,11 +15,16 @@ mv -f theme-temp/luci-theme-opentomcat package/lean/
 rm -rf theme-temp
 default_theme='opentomcat'
 sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
-# Add luci-app-vssr
+# Add luci-app-vssr passwall
 git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
+git clone https://github.com/xiaorouji/openwrt-passwall.git package-temp/passwall
 git clone https://github.com/kenzok8/small.git package-temp/small
 cp -r package-temp/small/* package/lean/
 mv -f package-temp/lua-maxminddb package/lean/
 mv -f package-temp/luci-app-vssr package/lean/
+mv -f package-temp/passwall package/lean/
 rm -rf package-temp
+
+#Add passwall 
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package-temp/passwall
